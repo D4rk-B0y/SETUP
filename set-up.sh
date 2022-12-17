a89f84ad39f26c7fd67a78671baed4ce27eced18
Md. Abedur Rahman <110812251+D4rk-B0y@users.noreply.github.com>
2022-12-17 16:45:18 +0600

    Add files via upload

---
+++ set-up.sh
@@ -0,0 +1,222 @@
+#!/bin/bash
+#if you copy this script, remember you will be my son 
+#and I'm a fucker of your mom
+
+
+clear
+
+
+clear
+
+echo "       Follow My Github Account " 
+echo ""
+echo " https://github.com/D4rk-B0y" 
+echo " "
+echo "           ♤𝐀𝐁𝐈𝐑 ❤ 𝐌𝐀𝐈𝐒𝐇𝐀♤"
+echo " "
+echo " Author : 𝐀𝐁𝐈𝐑 𝐑𝐀𝐇𝐌𝐀𝐍 "
+ xdg-open https://github.com/D4rk-B0y 
+sleep 2 
+
+echo ""------------------------------------------------------""
+# we are Anonymous
+
+ sleep 0.19
+echo ""
+echo ""
+
+ echo " If You Connected To FAST Internet Then  "
+sleep 0.19
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+sleep 0.19
+echo ""
+echo ""
+echo ""------------------------------------------------------""
+ sleep 1
+ sleep 0.19
+ echo  "#####################################"
+echo ""
+echo ""
+ echo  " FOLLOW ME  ON FACEBOOK "
+echo ""
+
+ echo  "#####################################"
+
+ sleep o.19
+
+ sleep 1
+
+  clear
+
+echo  "We are Anonymous,"
+sleep 0.19
+echo "  We are Poor, "
+sleep 0.19
+echo "  We do not forget,"
+sleep 0.19
+echo "  We Support Palestine,"
+sleep 0.19
+echo "  Expect Us. "
+sleep 0.19
+
+echo $green
+echo ""------------------------------------------------------""
+echo ""
+
+echo " ASSALAMU ALAIKUM "
+echo ""
+echo ""------------------------------------------------------ ""
+echo
+
+sleep 3
+
+clear
+
+
+echo  "#####################################"
+echo "  I AM VERY HAPPY "
+echo "  I LOVE MY MOTHER"
+echo "  I LOVE MY WIFE"
+echo "  I LOVE MY AUNT "
+echo "  I LOVE MY CHILDREN"
+echo "  I LOVE MY COUNTRY"
+echo "  I LOVE MY PROFESSION"
+
+echo ""
+
+echo  "#####################################"
+echo
+sleep 5
+
+clear
+
+echo  "#####################################"
+echo ""
+echo " follow me On Facebook "
+echo ""
+echo   "https://www.facebook.com/bap.dakbi "
+echo " "
+echo  "#####################################"
+sleep 3
+ xdg-open https://github.com/D4rk-B0y 
+
+echo
+
+
+echo " support me , share this tool for your friemds "
+
+sleep 8
+
+clear
+
+clear
+
+
+echo
+echo
+echo
+echo
+
+echo "."
+
+echo " starting package installation ... "
+echo " wait 5 sec  "
+sleep 5
+
+pkg update -y                     
+pkg upgrade -y
+pkg install figlet -y
+pkg install toilet -y
+clear
+pkg install sl
+pkg install libcaca
+pkg install  cmatrix
+clear
+
+clear
+
+pkg Install while
+clear
+
+clear
+pkg install python -y          
+pkg install python2 -y
+pkg install python3 -y
+clear
+pkg install git -y
+clear
+
+
+pkg install php -y 
+clear
+pkg install nmap -y
+clear
+
+
+pkg install bash -y
+clear
+
+
+pkg install nano -y
+pkg install curl -y
+
+clear
+
+pkg install wget -y
+
+clear
+
+pkg install openssl
+
+clear
+pip install requests 
+pip2 install requests 
+clear 
+
+pip install rich 
+pip2 install rich 
+clear
+
+pip install mechanize 
+pip2 install mechanize 
+clear
+
+pip install futures 
+pip2 install futures 
+clear
+
+pkg install pip -y
+
+pip install -- upgrade pip
+
+pip install lolcat
+pip2 install lolcat
+lolcat -h
+
+sleep 1
+
+clear
+
+pkg install python3-pip
+
+pkg install wireless-tools
+
+pkg install root-repo
+
+
+echo " 46 packages  install successfully "
+figlet BYEE  |lolcat -S 143
+
+
+
+
+exit 
+exit
+exit
+
+
+
+
+
+
+
